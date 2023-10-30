42bc0b4928fa42a183874d6eccd402ef1237feeb
Ryo Nakamaru <pottava@gmail.com>
2023-10-30 06:00:14 +0000

    Make Raspberry Pi to use environment variables

--- raspberry-pi/main.py
+++ raspberry-pi/main.py
@@ -2,26 +2,16 @@ import serial
 import random
 import time
 import sys, traceback
+import os
 from google.cloud import firestore
 
 leds = 512*8
 
-db = firestore.Client(project="mofilabs-next-demo-02")
+database = os.environ.get('FIRESTORE_DB', '(default)')
+collection = os.environ.get('LED_COLLECTION', 'led')
+serial_port = os.environ.get('SERIAL_PORT', '/dev/ttyACM0')
 
-def randomColors():
-    colorStr = []
-
-    for i in range(leds):
-        r = random.randint(0,50)
-        g = random.randint(0,50)
-        b = random.randint(0,50)
-        colorStr.append(r)
-        colorStr.append(g)
-        colorStr.append(b)
-
-    return bytearray(colorStr)
-
-
+db = firestore.Client(project=os.environ.get('PROJECT_ID', 'default'), database=database)
 
 def clear():
     colorStr = []
@@ -36,7 +26,7 @@ def clear():
 
 def get_byte_data():
     # Reference the specific document
-    doc_ref = db.collection('led').document('data')
+    doc_ref = db.collection(collection).document('data')
 
     # Fetch the document
     doc = doc_ref.get()
@@ -56,8 +46,8 @@ def get_byte_data():
 
 def main():
     try:
-        ser = serial.Serial('/dev/ttyACM0', 9600)
-        doc_ref = db.collection("led").document("data")
+        ser = serial.Serial(serial_port, 9600)
+        doc_ref = db.collection(collection).document("data")
         while True:
             color = get_byte_data()
             ser.write(color)
@@ -73,7 +63,6 @@ def main():
     except Exception:
         traceback.print_exc(file=sys.stdout)
     sys.exit(0)
-
 
 
 if __name__ == '__main__':
